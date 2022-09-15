856ed729547a8062c782ce6b4b05f92f57490444
Akhilesh <akhilesh.s@affle.com>
2022-09-15 11:33:53 +0530

    category-complete

--- src/Todos.js
+++ src/Todos.js
@@ -4,16 +4,12 @@ import { Link } from "react-router-dom";
 
 const Todos = () => {
   useEffect(() => {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
     if (!token) {
       window.location.href = "/register";
     }
   }, []);
-  return (
-    <div>
-      <Link to="category">Go to category</Link>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Todos;
